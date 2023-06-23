45a0cd88d8eb461db17a91888f2ba041b10230df
Serhii <116863810+Serhii-K5@users.noreply.github.com>
2023-06-23 18:08:24 +0300

    creating components
    Створено можливі компоненти

---
+++ src/utils/api/api.jsx
@@ -0,0 +1,23 @@
+// import axios from 'axios';
+
+// axios.defaults.baseURL = 'https://';
+
+// export const getProduct = async () => {
+//   const response = await axios.get(`/... ?...`);
+//   return response.map(({  }) => {
+//     return {
+      
+//     };
+//   });
+// };
+
+// export const getSearch = async inputValue => {
+//   const response = await axios.get(
+//     `/...?...&query=${inputValue}&language=en-US&page=1`
+//   );
+//   return response.map(({  }) => {
+//     return {
+      
+//     };
+//   });
+// };
---
+++ src/utils/hooks/blank.jsx
@@ -0,0 +1,11 @@
+// import { useState, useEffect } from 'react';
+
+// export const use___ = () => {
+//   const [value, setValue] = useState([]);
+
+//   useEffect(() => {
+    
+//   }, []);
+
+//   return { value };
+// }; 
